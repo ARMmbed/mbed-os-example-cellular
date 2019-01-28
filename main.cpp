806dd02af0dcdf7f4a6219c81f83172c8de38d47
Teppo Järvelin <teppo.jarvelin@arm.com>
2019-01-28 15:43:44 +0200

    Added calls to set PSM after disconnect to test PSM.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@
 #include "common_functions.h"
 #include "UDPSocket.h"
 #include "CellularLog.h"
+#include "CellularContext.h"
+#include "CellularPower.h"
 
 #define UDP 0
 #define TCP 1
@@ -229,6 +231,13 @@ int main()
         print_function("\n\n disconnect failed.\n\n");
     }
 
+    ////// START TEST PSMP    /////////
+    wait(5);
+    CellularContext *ctx = (CellularContext *)iface;
+    CellularPower *pwr = ctx->get_device()->open_power();
+    retcode = pwr->opt_power_save_mode(40, 4);
+    while(1);
+    ////// END TEST PSMP    /////////
     if (retcode == NSAPI_ERROR_OK) {
         print_function("\n\nSuccess. Exiting \n\n");
     } else {
